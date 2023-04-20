bb46d6be09bdf7ffc9940feccc3a748e011c605b
Ramesh Doddi <rdoddi.pro@gmail.com>
2023-04-19 21:49:17 -0700

    Initial commit.

---
+++ src/main/java/net/iremote/thiru/ThiruApplication.java
@@ -0,0 +1,13 @@
+package net.iremote.thiru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiruApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThiruApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/iremote/thiru/ThiruApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iremote.thiru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiruApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
